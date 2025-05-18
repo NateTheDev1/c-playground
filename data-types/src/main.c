ec0a82ae7620ad3cb7c934bd14e3c2016883607c
Nathaniel Richards <nthnlrichards@gmail.com>
2025-05-18 09:57:51 -0400

    Finished data types

---
+++ data-types/src/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    // char myCharacter = '\n';
+
+    // printf("%c", myCharacter);
+
+    // enum gender {male, female};
+
+    // enum gender myGender;
+
+    // myGender = male;
+
+    // enum gender anotherGender = female;
+
+    // bool isMale = (myGender== anotherGender);
+
+    // printf("Hello world!");
+    
+    // float jason = 23.333;
+    // double jason2 =  55.55555e+11;
+
+    // char broiled;
+    // broiled = 'T';
+    // broiled = "T"; Not Vaid, double quotes are a string
+
+    // char newLine = '\n';
+
+    // printf(newLine);
+
+    // bool isOn = false;
+    // _Bool otherTypeOfBool = 0;
+
+    return  0;
+}
--- intro/src/main.c
+++ intro/src/main.c
@@ -2,13 +2,30 @@
 
 int main() 
 {
-    char name[100];
-
-    printf("What is your name?\n");
-
-    scanf("%99s", name);
-
-    printf("Nice to meet you %s\n", name);
     
+
+    // char str[100];
+    // int i;
+
+    // printf("Enter a value :");
+
+    // scanf("%d %s", &i, str);
+
+    // printf("\nYou entered %d:::::%s\n", i, str);
+
+    // printf("\nWhat is Pi?\n");
+
+    // double x; // Ex. 
+
+    // scanf("%lf", &x);
+
+    // printf("\n You entered %lf\n", x);
+
+    // if(x == 3.14) {
+    //    printf("\nYou are right!\n");
+    // } else {
+    //     printf("\nYou were wrong...\n");
+    // }
+
     return 0;
 }
